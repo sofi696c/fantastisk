11ea46d05e0c826c431cf820128052b599c0b968
Sofie <soffis02@easv365.dk>
2024-05-23 12:44:06 +0200

    carousel

--- fantasyquest/src/main.js
+++ fantasyquest/src/main.js
@@ -7,3 +7,6 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
--- fantasyquest/src/views/HomeView.vue
+++ fantasyquest/src/views/HomeView.vue
@@ -1,13 +1,216 @@
 <template>
-    <div>
+    <main>
+      <div id="demo" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
 
-    </div>
-</template>
+          <div class="carousel-item active">
+            <img src="../assets/b1.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
 
-<script setup>
+          <div class="carousel-item">
+            <img src="../assets/kæmperne.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Mød de fire kæmper</h3>
+                <p>Kæmperne er fire store, stærke og modige helte, som er udvalgt til at redde verden fra de onde magter.
+                  <br><br>Deltag i årets Quest og hjælp dem!
+                </p>
+              </div>
+            </div>
+          </div>
 
-</script>
+          <div class="carousel-item">
+            <img src="../assets/b2.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
 
-<style lang="scss" scoped>
+          <div class="carousel-item">
+            <img src="../assets/b3.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
 
-</style>
+          <div class="carousel-item">
+            <img src="../assets/b4.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="../assets/b5.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="../assets/b6.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="../assets/b7.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="../assets/b8.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <img src="../assets/b9.jpg" alt="img" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+              <div class="text-box-left">
+                <h3>Velkommen til Fantasy Quest Esbjerg</h3>
+                <p>Her på siden finder du alle relevante informationer om årets Quest.
+                  <br> Vælg imellem en svær aftenudgave eller en familievenlig dagudgave under Fantasyfestivalen.
+                  <br><br>Vi glæder os til at se dig!
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+  
+        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </button>
+      </div>
+    </main>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style lang="scss" scoped>
+  main {
+    height: 500vh;
+  }
+  
+  /* Stilen for hele karousellen */
+  .carousel {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+  
+  /* Overlay gradient på hver karouselbillede */
+  .carousel-item::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+    width: 150vh;
+  }
+  
+  /* Positionering og styling af karouselltekster */
+  .carousel-caption {
+    position: absolute;
+    bottom: 35%;
+    left: 50px;
+    display: inline-block;
+  }
+  
+  /* Stilen for tekstboksen til venstre i karousellen */
+  .text-box-left {
+    text-align: left;
+    color: #F1C40F;
+    padding: 10px;
+    width: auto;
+    max-width: 500px;
+    border-radius: 5px;
+  }
+  
+  /* Stilen for overskriften i tekstboksen */
+  .text-box-left h3 {
+    font-size: 25px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  
+  /* Stilen for teksten i tekstboksen */
+  .text-box-left p {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+  
+  /* Størrelsen på billederne i karousellen */
+  .w-100 {
+    width: 100%;
+    height: 100vh;
+  }
+  </style>
+  
